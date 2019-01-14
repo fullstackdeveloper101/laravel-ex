b007abe3c0cd3cc10aae439215d2ca479bed95c1
Vinu Mohan <fullstackdeveloper101@gmail.com>
2019-01-14 05:20:50 -0800

    home page style modified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,31 @@
 @section('content')
 <div class="container">
     <div class="row">
+
+
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
                     You are logged in!
-                </div>
-                @if(\Session::has('success'))
+                    <div class="panel-body">
+                    @if(\Session::has('success'))
         <div class="alert alert-success">
             {{\Session::get('success')}}
         </div>
     @endif
+
+    <a href="{{url('/create/ticket')}}" class="btn btn-success">Create Ticket</a>
+       <a href="{{url('/tickets')}}" class="btn btn-default">All Tickets</a>
+       </div>
+                </div>
+
             </div>
         </div>
+
+
     </div>
-    <div class="row">
-       <a href="{{url('/create/ticket')}}" class="btn btn-success">Create Ticket</a>
-       <a href="{{url('/tickets')}}" class="btn btn-default">All Tickets</a>
-    </div>
+
 </div>
 @endsection
